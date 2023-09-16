eb55d4eefcb60050a633ffa3d83ee8c407b11032
sebastien lau <sebastien.lau2021@gmail.com>
2023-09-16 18:53:09 +0200

    config yml

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+require("dotenv").config();
 
 //********** import des routes **********
 const mealRoute = require("./routes/meal");
 const orderRoute = require("./routes/order");
 const adminRoute = require("./routes/admin");
 
-const mongoKey = "TqZih5ejnapcC9F";
+const mongoURL = process.env.MONGO_URL;
 
 mongoose
-	.connect(
-		`mongodb+srv://lausebastien:${mongoKey}@cluster0.je84pvt.mongodb.net/?retryWrites=true&w=majority`,
-		{ useNewUrlParser: true, useUnifiedTopology: true }
-	)
+	.connect(`${mongoURL}`, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() => console.log("Connexion à MongoDB réussie !"))
 	.catch(() => console.log("Connexion à MongoDB échouée !"));
 
@@ -23,11 +21,11 @@ app.use((req, res, next) => {
 	res.setHeader("Access-Control-Allow-Origin", "*");
 	res.setHeader(
 		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+		"Origin, X-Requested-With, Content, Accept, Content-Type, Authorization",
 	);
 	res.setHeader(
 		"Access-Control-Allow-Methods",
-		"GET, POST, PUT, DELETE, PATCH, OPTIONS"
+		"GET, POST, PUT, DELETE, PATCH, OPTIONS",
 	);
 	next();
 });
