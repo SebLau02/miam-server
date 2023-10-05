2b11a67ae0e99cdae8b1abd1d730ceea2345c0db
sebastien lau <sebastien.lau2021@gmail.com>
2023-10-05 19:39:54 +0200

    ajout de socket.io au serveur

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
 const http = require("http");
 const app = require("./app");
+const express = require("express");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+	cors: {
+		origin: "*",
+		methods: ["GET", "POST"],
+	},
+});
+
+io.on("connection", (socket) => {
+	console.log(`User Connected: ${socket.id}`);
+
+	socket.on("send_message", (data) => {
+		console.log(data);
+		socket.broadcast.emit("receive_message", data);
+	});
+});
 
 const normalizePort = (val) => {
 	const port = parseInt(val, 10);
@@ -12,7 +33,8 @@ const normalizePort = (val) => {
 	}
 	return false;
 };
-const port = normalizePort(process.env.PORT || "3000");
+
+const port = normalizePort(process.env.PORT || "3001");
 app.set("port", port);
 
 const errorHandler = (error) => {
@@ -35,8 +57,6 @@ const errorHandler = (error) => {
 			throw error;
 	}
 };
-
-const server = http.createServer(app);
 
 server.on("error", errorHandler);
 server.on("listening", () => {
